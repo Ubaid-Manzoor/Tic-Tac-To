5d6193381cc63c28d68d7f4c093842c247624f81
Ubaid Manzoor <deadhacker014@gmail.com>
2018-05-03 19:44:49 +0530

    done

--- script.js
+++ script.js
@@ -40,8 +40,17 @@ const GameBoard = ( ()=>{
         let Total_Boxes_Got_Filled = 0;
         let firstPlayerName = document.getElementById('firstPlayer');
         let secondPlayerName = document.getElementById('secondPlayer');
+        let info_about_user = document.getElementsByClassName('info_of_user')[0];
+        console.log(firstPlayerName.value);
+        //Form Validation
+        if(firstPlayerName.value ==="" || secondPlayerName.value ===""){
+            alert("Fill The Form");
+            return false;
+        }
         const Player1 = PlayerFactory("firstPlayerName","o");
         const Player2 = PlayerFactory("secondPlayerName","x");
+        info_about_user.innerHTML = '<p>'+firstPlayerName.value+' playing...</p>';
+        info_about_user.style.margin = "2px auto";
         //
 
         ResetAll = ()=>{
@@ -80,9 +89,13 @@ const GameBoard = ( ()=>{
                             if(Check_The_Winning_Status("0")){
                                 console.log("Player1 Won");
                                 Total_Boxes_Got_Filled = 0;
+                                info_about_user.innerHTML = '<p>'+firstPlayerName.value+' Won...</p>';
+                                console.log(info_about_user);
                                 ResetAll();
+                                return true;
                             }
                         }
+                        info_about_user.innerHTML = '<p>'+secondPlayerName.value+' playing...</p>';
                     }
                     else if(Player2.Chance === "0" && Box.innerHTML === "<h1> </h1>"){
                         //Change in Board Array
@@ -98,16 +111,29 @@ const GameBoard = ( ()=>{
                             if(Check_The_Winning_Status("1")){
                                 console.log("Player2 Won");
                                 Total_Boxes_Got_Filled = 0;
+                                info_about_user.innerHTML = '<p>'+secondPlayerName.value+' Won...</p>';
                                 ResetAll();
+                                return true;
                             }
                         }
+                        info_about_user.innerHTML = '<p>'+firstPlayerName.value+' playing...</p>';
                     }
                     if(Total_Boxes_Got_Filled == 9){
                         console.log("Draw");
                         Total_Boxes_Got_Filled = 0;
+                        info_about_user.innerHTML = '<p>Draw...</p>';
                         ResetAll();
+                        return true;
                     }
                 });
             });
      };
+     const reset_button = document.getElementsByClassName('Reset_button')[0];
+     console.log(reset_button);
+     reset_button.onclick = ()=>{
+        let info_about_user = document.getElementsByClassName('info_of_user')[0];
+        let firstPlayerName = document.getElementById('firstPlayer');
+         ResetAll();
+         info_about_user.innerHTML = '<p>'+firstPlayerName.value+' playing...</p>';
+     }
 })();
